cfd1c1693f97f4cb87d53cb214d7de07244b8946
Lenyn Conde <streetke47@gmail.com>
2023-09-28 16:42:24 -0400

    Home (biografia)

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,52 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
+import imagen_perfil from './images/imagen_perfil.png';
 
 function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
-      <Card>
-        <Card.Body>
-          <Card.Title>Contenido de la tarjeta</Card.Title>
-          <Card.Text>
-            Este es el contenido de la tarjeta vinculada a la página de inicio.
-          </Card.Text>
-        </Card.Body>
-      </Card>
+      <div className="card-container">
+        <div
+          className="image-container"
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <img
+            src={imagen_perfil}
+            alt="Imagen"
+            style={{
+              width: '20%',
+              borderRadius: '50%',
+              marginTop: '40px',
+            }}
+          />
+        </div>
+        <div className="card-text-container" style={{ marginTop: '-40px' }}>
+          <Card style={{ transform: 'scale(0.75)' }}>
+            <Card.Body>
+              <Card.Title style={{ textAlign: 'justify', fontSize: '28px'}}><strong>Biografía</strong></Card.Title>
+              <Card.Text>
+                <p style={{ textAlign: 'justify', fontSize: '20px'}}><strong>Nacimiento y Origen</strong></p>
+                <p style={{ textAlign: 'justify', fontSize: '20px' }}>Lenyn Conde, nacido el 19 de noviembre de 1990 en la vibrante ciudad de Valencia, Venezuela, es un individuo multifacético que ha dedicado su vida a la educación, la tecnología y el bienestar físico.</p>
+                <p style={{ textAlign: 'justify', fontSize: '20px' }}><strong>Educación y Carrera Profesional</strong></p>
+                <p style={{ textAlign: 'justify', fontSize: '20px' }}>Lenyn Conde se graduó como Licenciado en Educación con mención en Matemáticas en la Universidad de Carabobo, destacándose como un apasionado educador desde el principio de su carrera.</p>
+                <p style={{ textAlign: 'justify', fontSize: '20px' }}>Además de su pasión por la educación, Lenyn es un cocinero profesional egresado de la Escuela Culinaria Le Gourmet's.</p>
+                <p style={{ textAlign: 'justify', fontSize: '20px' }}>Actualmente, Lenyn Conde está cursando una especialización en Telemática e Informática en la Educación a Distancia en la Universidad Nacional Abierta. Este camino académico refleja su compromiso constante con la mejora de la educación a través de la tecnología.</p>
+                <p style={{ textAlign: 'justify', fontSize: '20px' }}>Además de sus logros académicos y culinarios, Lenyn es un amante apasionado de la calistenia, una disciplina de entrenamiento que utiliza el peso corporal para desarrollar la fuerza y la agilidad. Su dedicación a la calistenia refleja su compromiso con el bienestar físico y la salud.</p>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default Home;
+
+
+
+

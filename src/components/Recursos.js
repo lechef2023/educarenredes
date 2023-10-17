2c504b11d0ec4e513807ce805eb702b0993711a1
lechef2023 <streetke47@gmail.com>
2023-10-17 17:01:43 -0400

    Video herramientas Digitales

--- src/components/Recursos.js
+++ src/components/Recursos.js
@@ -7,6 +7,40 @@ function Recursos() {
     <div>
       <Accordion defaultActiveKey="0">
         <Accordion.Item eventKey="0">
+          <Accordion.Header>Herramientas de Comunicación</Accordion.Header>
+          <Accordion.Body style={{ background: '#CCF4F9' }}>
+            <div className="card">
+              <div className="card-header text-center">
+                <h5 className="card-title">Herramientas de Comunicación</h5>
+              </div>
+              <div className="card-body">
+                <div className="card-content text-center">
+                  <div className="embed-responsive embed-responsive-16by9">
+                    <iframe
+                      className="embed-responsive-item"
+                      src="https://www.youtube.com/embed/5bBq4rX5mtE"
+                      title="Video de Conceptos Básicos"
+                      frameBorder="0"
+                      allowFullScreen
+                    ></iframe>
+                  </div>
+                </div>
+                <p className="card-text" style={{ textAlign: 'justify', fontSize: '18px', marginTop: '20px' }}>
+                  En este video, te sumergirás en el emocionante mundo de la comunicación sincrónica y asincrónica. Exploraremos las herramientas esenciales que facilitan la comunicación en tiempo real y las que permiten interacciones flexibles en tu propio horario.
+
+                  Descubre cómo las herramientas de comunicación sincrónica, como el chat en vivo y las videoconferencias, te permiten conectarte con personas en todo el mundo al instante. Aprenderás a aprovechar estas herramientas para reuniones en línea, colaboración en tiempo real y la creación de comunidades virtuales.
+
+                  Luego, exploraremos las herramientas de comunicación asincrónica, como el correo electrónico, los foros y las listas de correo, que te brindan la flexibilidad de comunicarte cuando mejor te convenga. Descubrirás cómo utilizar estas herramientas para mantener conversaciones significativas, compartir recursos y trabajar en proyectos de manera colaborativa.
+
+                  No importa si eres un estudiante, un profesional o simplemente alguien interesado en aprovechar al máximo la comunicación en línea, este video te brindará una visión completa de las herramientas que tienes a tu disposición.
+
+                  Optimiza tu comunicación en línea y lleva tus interacciones a un nivel superior. ¡Acompáñanos en este emocionante viaje por las herramientas de comunicación sincrónica y asincrónica!
+                </p>
+              </div>
+            </div>
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
           <Accordion.Header>Presentación Interactiva</Accordion.Header>
           <Accordion.Body style={{ background: '#CCF4F9' }}>
             <div className="card">
@@ -26,7 +60,7 @@ function Recursos() {
             </div>
           </Accordion.Body>
         </Accordion.Item>
-        <Accordion.Item eventKey="1">
+        <Accordion.Item eventKey="2">
           <Accordion.Header>Conceptos Básicos</Accordion.Header>
           <Accordion.Body style={{ background: '#CCF4F9' }}>
             <div className="card">
@@ -49,7 +83,7 @@ function Recursos() {
             </div>
           </Accordion.Body>
         </Accordion.Item>
-        <Accordion.Item eventKey="2">
+        <Accordion.Item eventKey="3">
           <Accordion.Header>Introducción a Redes</Accordion.Header>
           <Accordion.Body style={{ background: '#CCF4F9' }}>
             <div className="card">
@@ -72,7 +106,7 @@ function Recursos() {
             </div>
           </Accordion.Body>
         </Accordion.Item>
-        <Accordion.Item eventKey="3">
+        <Accordion.Item eventKey="4">
           <Accordion.Header>Funcionamiento</Accordion.Header>
           <Accordion.Body style={{ background: '#CCF4F9' }}>
             <div className="card">
@@ -95,7 +129,7 @@ function Recursos() {
             </div>
           </Accordion.Body>
         </Accordion.Item>
-        <Accordion.Item eventKey="4">
+        <Accordion.Item eventKey="5">
           <Accordion.Header>Estructura</Accordion.Header>
           <Accordion.Body style={{ background: '#CCF4F9' }}>
             <div className="card">
